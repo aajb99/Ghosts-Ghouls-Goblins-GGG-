e162cb767391b43c26c11218359cc115bf6fcef6
Aaron Brown <aaron.jacob.brown@gmail.com>
2023-11-06 14:10:15 -0700

    GGG NN method

--- GhostsGhoulsGoblins.R
+++ GhostsGhoulsGoblins.R
@@ -29,6 +29,7 @@ library(themis)
 
 data_train <- vroom("./data/train.csv") %>%
   mutate(type=factor(type))# grab training data
+# Missing values fill experiment:
 miss_val_data <- vroom("./data/trainWithMissingValues.csv") %>%
   mutate(type=factor(type))# grab training data
 
@@ -59,7 +60,7 @@ rmse_vec(data_train[is.na(miss_val_data)], baked_data1[is.na(miss_val_data)])
 my_recipe <- recipe(rFormula, data = data_train) %>% # set model formula and dataset
   #step_mutate_at(all_numeric_predictors(), fn = factor) %>%
   #step_other(all_nominal_predictors(), threshold = .001) %>%
-  step_lencode_glm(color, outcome = vars(type)) %>% # get hours
+  step_lencode_glm(color, outcome = vars(type)) %>% 
   #step_pca(all_predictors(), threshold = 0.8) # Threshold between 0 and 1, test run for classification rf
   #step_smote(all_outcomes(), neighbors = 5)
   step_dummy(all_nominal_predictors()) # get dummy variables
@@ -116,8 +117,52 @@ vroom_write(ggg_predictions, "./data/ggg_pred_knn.csv", delim = ",")
 # load('amazon_knn_wf.RData')
 
 
+###########################
+##### Neural Networks #####
+###########################
 
+nn_recipe <- recipe(rFormula, data= data_train) %>%
+  update_role(id, new_role="id") %>%
+  step_lencode_glm(color, outcome = vars(type)) %>% ## Turn color to factor then dummy encode color
+  step_dummy(all_nominal_predictors()) %>%
+  step_range(all_numeric_predictors(), min=0, max=1) #scale to [0,1]
 
+nn_model <- mlp(hidden_units = tune(),
+                epochs = 50, #or 100 or 250
+                activation="relu") %>% # RELU works comparatively well
+  set_engine("keras", verbose=0) %>% #verbose = 0 prints off less
+  set_mode("classification")
+
+nn_wf <- workflow() %>%
+  add_recipe(nn_recipe) %>%
+  add_model(nn_model)
+
+nn_tuneGrid <- grid_regular(hidden_units(range=c(1, 10)),
+                            levels=5)
+
+# Split data for CV
+folds <- vfold_cv(data_train, v = 10, repeats = 1)
+
+# Run CV
+CV_results <- nn_wf %>%
+  tune_grid(resamples = folds,
+            grid = nn_tuneGrid,
+            metrics = metric_set(accuracy))
+
+bestTune <- CV_results %>%
+  select_best('accuracy')
+
+final_wf <- nn_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = data_train)
+
+# MLP in terms of accuracy
+tuned_nn %>% collect_metrics() %>%
+  filter(.metric=="accuracy") %>%
+  ggplot(aes(x=hidden_units, y=mean)) + geom_line()
+
+## CV tune, finalize and predict here and save results22
+## This takes a few min (10 on my laptop) so run it on becker if you want
 
 
 
